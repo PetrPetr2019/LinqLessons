3df7a3e69a16c07e17c9f69010ccef19e1a38704
Petr <ferel2015@yandex.ru>
2020-03-28 16:34:12 +0300

    Query Using Linq

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LinqTestLessons
@@ -7,31 +9,41 @@ namespace LinqTestLessons
     {
         static void Main(string[] args)
         {
-
-            using (appContext db = new appContext())
+            // вызов LINQ с применение унаследованных коллекций 
+            var arrayList = new ArrayList();
+            arrayList.Add("Adam");
+            arrayList.Add("Jon");
+            arrayList.Add("Tom");
+            var names = arrayList.Cast<string>().Where(n => n.Length < 4);
+            foreach (var result in names)
             {
-                Employee employee1 = new Employee { firstName = "Tom", LasName = "Jemson" };
-                Employee employee2 = new Employee { firstName = "Bil", LasName = "Henks" };
-
-                db.employees.AddRange(employee1, employee2);
-                db.SaveChanges();
-                Console.WriteLine("Done");
-
+                Console.WriteLine(result);
             }
+            //using (appContext db = new appContext())
+            //{
+            //    Employee employee1 = new Employee { firstName = "Tom", LasName = "Jemson" };
+            //    Employee employee2 = new Employee { firstName = "Bil", LasName = "Henks" };
 
-            using (appContext db = new appContext())
-            {
-                var Query = db.employees.Select(t => new 
-                {
-                    Name = t.LasName,
-                    Family = t.firstName,
-                    ID = t.id
-                });
-                foreach (var result in Query)
-                {
-                    Console.WriteLine($"Имя:{result.Name} - Фамилия:{result.Family} - ID:{result.ID}");
-                }
-            }
+            //    db.employees.AddRange(employee1, employee2);
+            //    db.SaveChanges();
+            //    Console.WriteLine("Done");
+
+            //}
+
+            //using (appContext db = new appContext())
+            //{
+               
+            //    //var Query = db.employees.Select(t => new 
+            //    //{
+            //    //    Name = t.LasName,
+            //    //    Family = t.firstName,
+            //    //    ID = t.id
+            //    //});
+            //    //foreach (var result in Query)
+            //    //{
+            //    //    Console.WriteLine($"Имя:{result.Name} - Фамилия:{result.Family} - ID:{result.ID}");
+            //    //}
+            //}
 
             Console.ReadLine();
         }
