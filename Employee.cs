a921f51b459ba88b10d6398a098527611545838a
Petr <ferel2015@yandex.ru>
2020-03-28 16:10:10 +0300

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinqTestLessons
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string firstName{ get; set; }
+        public string LasName { get; set; }
+
+    }
+
+    public class appContext:DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server = (localdb)\\MSSQLLocalDB; Database = LinqDB; Trusted_Connection = True");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTestLessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (appContext db = new appContext())
+            {
+                Employee employee1 = new Employee { firstName = "Tom", LasName = "Jemson" };
+                Employee employee2 = new Employee { firstName = "Bil", LasName = "Henks" };
+
+                db.employees.AddRange(employee1, employee2);
+                db.SaveChanges();
+                Console.WriteLine("Done");
+
+            }
+
+            using (appContext db = new appContext())
+            {
+                var Query = db.employees.Select(t => new 
+                {
+                    Name = t.LasName,
+                    Family = t.firstName,
+                    ID = t.id
+                });
+                foreach (var result in Query)
+                {
+                    Console.WriteLine($"Имя:{result.Name} - Фамилия:{result.Family} - ID:{result.ID}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
